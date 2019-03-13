92d5cf5ee21f04654439d543c6e007ff69c7bb59
dimitrifrazao <dimitrifrazao@users.noreply.github.com>
2019-03-13 13:03:13 -0700

    updating our link update script
    yes sounds redundant

--- scripts/updateLinks.js
+++ scripts/updateLinks.js
@@ -1,47 +1,27 @@
-function updateHeader(isIndex=false) {
+function updateHeader() {
 
-    if(isIndex){
-        $("img#logoLink").attr('src', "images/logo.png")
 
-        let pages = ["index.html", "../games.html", "../API.html", "../tutorial.html", "../about.html", "../contact.html"];
+    $("img#logoLink").attr('src', "../images/logo.png")
 
-        $('nav#pages a').each( function(index)
-        {
-            $(this).attr('href', pages[index]);
-        });
-        console.log("index header update");
+    let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
 
-    }
-    else{
-        $("img#logoLink").attr('src', "../images/logo.png")
-
-        let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
-
-        $('nav#pages a').each( function(index)
-        {
-            $(this).attr('href', pages[index]);
-        });
-    }
+    $('ul#pages a').each( function(index)
+    {
+        $(this).attr('href', pages[index]);
+    });
     
     console.log("header link set");
 }
 
-function updateFooter(isIndex=false){
+function updateFooter(){
 
-    imagePaths = ["images/face.png", "images/insta.png", "images/twitter.png"]
+    imagePaths = ["../images/face.png", "../images/insta.png", "../images/twitter.png"]
 
-    if(isIndex){
-        $('section.clearFlt img').each( function(index)
-        {
-            $(this).attr('src', imagePaths[index]);
-        });
-    }
-    else{
-        $('section.clearFlt img').each( function(index)
-        {
-            $(this).attr('src', "../" + imagePaths[index]);
-        });
-    }
+
+    $('section.clearFlt img').each( function(index)
+    {
+        $(this).attr('src', imagePaths[index]);
+    });
     
 
     console.log("footer link set");
