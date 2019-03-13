673e8f19e7b076ee620afeddb5962e226d4caeaf
dimitrifrazao <dimitrifrazao@users.noreply.github.com>
2019-03-13 12:45:58 -0700

    breaking update link
    making a header and footer version to be called afetr load()

--- scripts/updateLinks.js
+++ scripts/updateLinks.js
@@ -1,20 +1,48 @@
-function updatePageLinks() {
+function updateHeader(isIndex=false) {
 
-    $("img#logoLink").attr('src', "../images/logo.png")
+    if(isIndex){
+        $("img#logoLink").attr('src', "images/logo.png")
 
-    let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
+        let pages = ["index.html", "../games.html", "../API.html", "../tutorial.html", "../about.html", "../contact.html"];
 
-    $('nav#pages a').each( function(index)
-    {
-        $(this).attr('href', pages[index]);
-    });
+        $('nav#pages a').each( function(index)
+        {
+            $(this).attr('href', pages[index]);
+        });
+        console.log("index header update");
 
-    $('section.clearFlt img').each( function(index)
-    {
-        let imagePath = $(this).attr('src');
-        imagePath = "../" + imagePath;
-        $(this).attr('src', imagePath);
-    });
+    }
+    else{
+        $("img#logoLink").attr('src', "../images/logo.png")
 
-    console.log("Page link update Done!");
+        let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
+
+        $('nav#pages a').each( function(index)
+        {
+            $(this).attr('href', pages[index]);
+        });
+    }
+    
+    console.log("header link set");
+}
+
+function updateFooter(isIndex=false){
+
+    imagePaths = ["images/face.png", "images/insta.png", "images/twitter.png"]
+
+    if(isIndex){
+        $('section.clearFlt img').each( function(index)
+        {
+            $(this).attr('src', imagePaths[index]);
+        });
+    }
+    else{
+        $('section.clearFlt img').each( function(index)
+        {
+            $(this).attr('src', "../" + imagePaths[index]);
+        });
+    }
+    
+
+    console.log("footer link set");
 };
