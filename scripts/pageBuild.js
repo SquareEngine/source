d27b8cdc5736d9d40867b57c93bcce28d5732331
dimitrifrazao <dimitrifrazao@users.noreply.github.com>
2019-03-13 10:50:48 -0700

    pushing about page update
    pageBuild script now updates the pmage paths and page links

--- scripts/pageBuild.js
+++ scripts/pageBuild.js
@@ -1,75 +1,20 @@
 $(document).ready(function() {
-    /*
-    building the header
-    */
-    let logo = $('<a href="index.html" id="logo"></a>')
-        .append('<img src="https://i.imgur.com/3Z7ddcD.png" alt="Logo" width="60">');
 
-    let pathname = window.location.pathname;
-    let pageArray = pathname.split("/");
-    let page = pageArray[pageArray.length -1];
-    let isIndex = (page === "index.html");
-    let pages = ["index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
-    let pageText = ["Home", "Games", "API", "Tutorial", "About", "Contact"];
+    $("img#logoLink").attr('src', "../images/logo.png")
 
-    let ul = $('<ul></ul>');
-    $.each(pages, function(index)
+    let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
+
+    $('nav#pages a').each( function(index)
     {
-        let currentPage = pages[index];
-        let preString = "";
-        if(isIndex && index>0) preString = "pages/";
-        else if(isIndex==false && index==0) preString = "../"
-
-        let li = $('<li/>').appendTo( ul );
-        let lia = $('<a/>').attr('href', preString + currentPage).text( pageText[index] ).appendTo(li);
-        if(currentPage==page) lia.addClass('current');
+        $(this).attr('href', pages[index]);
     });
 
-    let header = $("<header></header>")
-        .append(logo)
-        .append( $("<nav></nav>")
-        .append(ul) );
-
-    /*
-    building main
-    */
-
-    let hero = $('<div id="hero"></div>');
-        
-    
-    /*
-    building footer
-    */
-
-    let sec = $('<section class="clearFlt"></section>');
-
-    let imageLinks = ["https://www.facebook.com", "https://www.instagram.com/?hl=en", "https://twitter.com"]
-    let imageImgu = ["https://i.imgur.com/DNxeV5O.png", "https://i.imgur.com/JLekVgr.png", "https://i.imgur.com/ZsurAxE.png"]
-    
-    $.each(imageLinks, function(index)
+    $('section.clearFlt img').each( function(index)
     {
-        let img = $('<img/>').attr( {'src':imageImgu[index], "width": 30});
-        let lia = $('<a/>').attr('href', imageLinks[index]).append(img);
-        sec.append(lia);
+        let imagePath = $(this).attr('src');
+        imagePath = "../" + imagePath;
+        $(this).attr('src', imagePath);
     });
-
-    sec.append( $('<p class="copy"> &copy 2019 </p>') );
-    
-
-    let footer = $("<footer></footer>").append(sec);
-    //footer.text("Dimitri Frazao")
-    
-
-     /*
-    add wrapper and append to body
-    */
-
-
-    $("body")
-        .append( $('<main id="wrapper"></div>')
-        .append(header)
-        .append(hero)
-        .append(footer) );
 
     console.log("Page Build Done");
 });
