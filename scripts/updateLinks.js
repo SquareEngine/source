e32b2ff766d747d5633822ee8746bedaf91a5243
dimitrifrazao <dimitrifrazao@users.noreply.github.com>
2019-03-13 22:30:30 -0700

    pushing more css
    made backgroun cube b&w

--- scripts/updateLinks.js
+++ scripts/updateLinks.js
@@ -8,21 +8,16 @@ function updateHeader(isIndex=false) {
     if(isIndex){
         logoPath = "images/logo.png";
         pages = ["index.html", "pages/games.html", "pages/API.html", "pages/tutorial.html", "pages/about.html", "pages/contact.html"];
-        console.log("is index");
     }
 
     
     $("a#logoLink").append('<img src="' + logoPath +  '"  alt="Logo" width="60">');
 
     $('ul#pages li').each( function(index)
-    {
-        
-        //let linkElement = $('<a href="' + pages[index] + '" class="current"> '+ linkNames[index] + ' </a>')
+    {      
         $(this).append('<a href="' + pages[index] + '" class="current"> '+ linkNames[index] + ' </a>')
-        //this.attr('href', pages[index]);
     });
     
-    console.log("header link set3");
 }
 
 function updateFooter(isIndex=false){
@@ -31,16 +26,12 @@ function updateFooter(isIndex=false){
     let imagePaths = ["../images/face.png", "../images/insta.png", "../images/twitter.png"]
     if(isIndex){
         imagePaths = ["images/face.png", "images/insta.png", "images/twitter.png"]
-        console.log("is index");
     }
 
     $('section.clearFlt a').each( function(index)
     {
-        //let imageElement = $('<img src="' + imagePaths[index] + '" alt="' + linkNames[index] + '" width="30"></a>')
         $(this).append('<img src="' + imagePaths[index] + '" alt="' + linkNames[index] + '" width="30"></a>')
-        //attr('src', imagePaths[index]);
     });
     
 
-    console.log("footer link set3");
 };
