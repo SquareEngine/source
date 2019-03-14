1cb1a1cf276a4c53a6e96f03444cb8542845c96e
dimitrifrazao <dimitrifrazao@users.noreply.github.com>
2019-03-13 22:39:56 -0700

    fixing logo link issue
    logo link now works

--- scripts/updateLinks.js
+++ scripts/updateLinks.js
@@ -3,16 +3,17 @@ function updateHeader(isIndex=false) {
     let linkNames = ["Home", "Games", "API", "Tutorial", "About", "Contact"]
 
     let logoPath = "../images/logo.png";
+    let logoLink = "../index.html";
     let pages = ["../index.html", "games.html", "API.html", "tutorial.html", "about.html", "contact.html"];
 
     if(isIndex){
         logoPath = "images/logo.png";
+        logoLink = "index.html";
         pages = ["index.html", "pages/games.html", "pages/API.html", "pages/tutorial.html", "pages/about.html", "pages/contact.html"];
     }
 
-    
+    $("div#logoDiv").append('<a href="' + logoLink + '" id="logoLink"></a>');
     $("a#logoLink").append('<img src="' + logoPath +  '"  alt="Logo" width="60">');
-
     $('ul#pages li').each( function(index)
     {      
         $(this).append('<a href="' + pages[index] + '" class="current"> '+ linkNames[index] + ' </a>')
