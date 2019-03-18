52b69a07b36aef80f263bd891a793157d3884092
tamiresboniolo <tamiresboniolo@users.noreply.github.com>
2019-03-17 18:48:52 -0700

    fix some design

--- games/breakOut.js
+++ games/breakOut.js
@@ -7,12 +7,17 @@
 /* Simple breakOut game using the squareEngine.
  Paddle moves right and left with the arrows control on the keyboard.
  Seeting ball colision, counting points and game over. 
+ 
+ This breakOut file needs our page to have loaded both these files to work:
+1 - squareEngine.js
+2 - JQuery.js
 */
 
 //gameGrid constants
 const CANVAS_SCALE = 26;
 const SPEED_UP = 1;
 
+//creating the grid of grid class (GameGrid)
 var gameGrid = new GameGrid(30, 20, CANVAS_SCALE);
 
 //showing the points on game over screen
@@ -21,7 +26,7 @@ gameGrid.gameOver = function(){
 }
 
 
-// JQuery events to handle buttons that affect the game
+// JQuery events to handle buttons (start, pause, reset) that affect the game
 $("#start").click( function(){ gameGrid._setGameToUpdate() });
 $("#pause").click(
     function(){
@@ -33,33 +38,33 @@ $("#reset").click( function(){ gameGrid._reset(); });
 $("input, select#mode").change( function(){ gameGrid._reset(); } );
 
 
-//start the game
+//function to start the game
 gameGrid.start = function(){
-
+	
+	//text on the screen before start to play
     gameGrid._startText = ["Enter: start", "Left arrow key: move left", "Right arrow key: move right"]
 	
     var screenHorizontalHalf = parseInt(gameGrid.getWidth()/2);
     var paddleVerticalPosition = gameGrid.getHeight()-0.5;
-    gameGrid.setBackgroundColor(RGB.makeRandomColor());
+    gameGrid.setBackgroundColor(RGB.makeRandomColor()); //ramdom background color
 
-    //keep the points on the screen
-    gameGrid.points = 0;
+    //print the points on the screen
+    gameGrid.points = 0; //starts with 0
     gameGrid.print("Your points: " + gameGrid.points);
 
-    //paddle starts on the middle of the screen
+    //creating the bar under the paddle (createGameObject)
     var bar = gameGrid.createGameObject("bar", "Basic", x=screenHorizontalHalf, y=paddleVerticalPosition);
-    bar.setColor(120,120,120);
+	bar.setColor(120,120,120); //grey
     bar.getSquare().size.x = gameGrid.getWidth();
 
-    // set paddle object
+    //creating the paddle object (createGameObject)
     var paddle = gameGrid.createGameObject("paddle", "Paddle", x=screenHorizontalHalf, y=paddleVerticalPosition);
 
+    //disabling paddle Up, down and around moviment	
     paddle.setWrapAroundOff();
-
-    //disabling paddle Up and down moviment
     paddle.setHorizontal()
-    paddle.setSpeed(15);
-    paddle.setColor(0,0,255);   
+    paddle.setSpeed(15);//speed of the paddle
+    paddle.setColor(0,0,255);  //blue
 
     paddle.getSquare().size.x = 5;
     paddle.updateBoundingBox();
@@ -68,25 +73,24 @@ gameGrid.start = function(){
 
     //creating the ball object of ball class (GameObjectBall)
     var ball = gameGrid.createGameObject("ball", "Ball", x=screenHorizontalHalf, y=paddleVerticalPosition - 1);
-    //ball.setUpdateDelay(1);
     ball.setDirection(-1, -1);
-    ball.setSpeed(15);
+    ball.setSpeed(15);//speed of the ball
     ball.botOffset += 1;
     ball.paddle = paddle;
 
-    //touch bottom game over or touch the paddle and return up
+    //colission: touch bottom game over or touch the paddle bounce and return up.
     ball.touchedBot = function(){
 
         let testPosition = this.getPosition().sum(new Vector(0, 1))
         if(this.paddle.checkVectorCollision( testPosition ) ){
-            //this.position.y = this.prevPosition.y;
             this.direction.y = 1;
             if(this.position.x > this.paddle.position.x) this.direction.x = 1;
             else this.direction.x = -1;
           }
-		  //gameOver text and r to restart the game
+		  
+		  //if ball touch bottom show gameOver text and r to reset the game
         else{
-            gameGrid._gameOverText = ["GAME OVER", "Press R to restart"];
+            gameGrid._gameOverText = ["GAME OVER", "Press R to reset"];
             this.gameOver();
         }
         
@@ -96,11 +100,12 @@ gameGrid.start = function(){
     var xCount = 9;
     var yCount = 3;
 
-    // create 3 random colors
+    // create random colors
     var rColor1 = RGB.makeRandomColor();
     var rColor2 = RGB.makeRandomColor();
     var rColor3 = RGB.makeRandomColor();
-
+	
+	//create the blocks grid
     for(let i =0; i<xCount; i++){
         for(let j =0; j<yCount; j++){
             // unique block name
@@ -112,11 +117,9 @@ gameGrid.start = function(){
             // lerp between our 3 random colors
             // check the RGB lerp method
             let resultColor = RGB.lerpColor(rColor1, rColor2, i/xCount); // i/xCount returns a scalar (0to1) for our lerp
-            resultColor = RGB.lerpColor(resultColor, rColor3, j/yCount); // j/yCount returns a scalar (0to1) for our lerp
-            // as we loop through xCount and our i increases if you test i/xCount it will start at 0 and get close to 1
-            // same for j/yCount
+            resultColor = RGB.lerpColor(resultColor, rColor3, j/yCount); // j/yCount returns a scalar (0to1) 
 
-            block.setColorObject(resultColor);
+            block.setColorObject(resultColor); //ramdom color on blocks
 
             for(let i=0; i<2; i++){
                 let square = new Square();
@@ -124,23 +127,23 @@ gameGrid.start = function(){
                 block.pushSquare(square);
             }
 			
-			//check the colision
+			//check the colision on the sides of the screen. Bounce off the walls.
             block.ball = ball;
             block.update = function(gameGrid){
                 let col = this.checkCollision( this.ball );
                 if( col ){
-                    //this.position.y = this.prevPosition.y;
                     if(col.size.x > col.size.y) this.ball.direction.y *= -1;
                     else this.ball.direction.x *= -1;
                     this.hide();
                     this.disableUpdate();
-                    //calculating the points
+					
+                    //calculating the points each time break a block
                     gameGrid.points += 1;
                     gameGrid.print("Your points: " + gameGrid.points);
 					
 					//if destroyed all the blocks will show the message that you won and the option to restart
                     if(gameGrid.points >= (xCount * yCount)){
-                        gameGrid._gameOverText = ["YOU WON!", "Press R to restart"];
+                        gameGrid._gameOverText = ["YOU WON!", "Press R to reset"];
                         this.gameOver();
                     }
                 }
@@ -151,9 +154,6 @@ gameGrid.start = function(){
 
 //loop the game
 createGameLoop(gameGrid);
-
-//text on the game grid
-document.getElementById("info").innerHTML = "Use SPACE to pause game. \nUse 'R' to restart.";
 
 
 
